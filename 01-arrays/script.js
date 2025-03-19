aa55d9d52e8b9fedd2905d4252c625fa3f2cee5a
simonelupone <simone.lupone@gmail.com>
2025-03-19 15:20:12 +0100

    [ex 6]

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -31,10 +31,11 @@ const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
-console.log(teachers);
-
+// console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah');
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
