743a1dcb0eeac4feb60432153b3ff4c3c77b1435
simonelupone <simone.lupone@gmail.com>
2025-03-19 15:14:14 +0100

    [ex 3]

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,14 +11,19 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
+// console.log(fourthTeacher);
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 // Carlo -> Patrick
-teachers.splice(4, 1, 'Patrick');
+// teachers.splice(4, 1, 'Patrick');
 // console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+
+console.log('last teacher:', lastTeacher);
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
